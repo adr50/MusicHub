7ec262da6452fec9795b6dabc71df586bad69c87
adr50 <56275859+adr50@users.noreply.github.com>
2019-12-02 14:27:24 -0500

    Update cron_update.php

--- Database Server/cron_update.php
+++ Database Server/cron_update.php
@@ -2,6 +2,7 @@
 
 $api = new rabbitMQClient("api.ini", "testServer"); // Might need a separate API receiver to avoid errors.
 $database = new mysqli("localhost", "root", "password", "website");
+
 $file = fopen("search_cache.txt", "r");
 $queries = array();
 
@@ -18,29 +19,29 @@ for (i = 0; i <= count($queries); i++) { // Iterate through each element.
     	$album_keys = array('id', 'title');
     	$track_keys = array('id', 'title', 'duration');
     	$passing = array();
-    	for($i = 0; $i < $assoc['total']; $i++){
-        	$artist_id_data = $assoc['data'][$i]['artist'][$artist_keys[0]];
-        	$artist_name_data = $assoc['data'][$i]['artist'][$artist_keys[1]];
-        	$album_id_data = $assoc['data'][$i]['album'][$album_keys[0]];
-        	$album_title_data = $assoc['data'][$i]['album'][$album_keys[1]];
-        	$track_id_data = $assoc['data'][$i][$track_keys[0]];
-        	$track_title_data = $assoc['data'][$i][$track_keys[1]];
-        	$track_duration_data = $assoc['data'][$i][$track_keys[2]];
+    	for($a = 0; $a < $assoc['total']; $a++){
+        	$artist_id_data = $assoc['data'][$a]['artist'][$artist_keys[0]];
+        	$artist_name_data = $assoc['data'][$a]['artist'][$artist_keys[1]];
+        	$album_id_data = $assoc['data'][$a]['album'][$album_keys[0]];
+        	$album_title_data = $assoc['data'][$a]['album'][$album_keys[1]];
+        	$track_id_data = $assoc['data'][$a][$track_keys[0]];
+        	$track_title_data = $assoc['data'][$a][$track_keys[1]];
+        	$track_duration_data = $assoc['data'][$a][$track_keys[2]];
         	if ($artist_id_data != "" || $artist_name_data != ""){
-        	$passing[$i] = array('artist_id'=> $artist_id_data, 'name'=> $artist_name_data, 
+        	$passing[$a] = array('artist_id'=> $artist_id_data, 'name'=> $artist_name_data, 
         	'album_id' => $album_id_data, 'album_title' => $album_title_data, 'track_id' => $track_id_data, 
       		'track_title' => $track_title_data, 'track_duration' => $track_duration_data);
 	}
 
-    	for ($i = 0; $i < count($passing); $i++) {
+    	for ($b = 0; $b < count($passing); $b++) {
     
-      		$artist_id = $passing[$i]['artist_id'];
-      		$name = $passing[$i]['name'];
-      		$album_id = $passing[$i]['album_id'];
-      		$album_title = $passing[$i]['album_title'];
-      		$track_id = $passing[$i]['track_id'];
-      		$track_title = $passing[$i]['track_title'];
-      		$track_duration = $passing[$i]['track_duration'];
+      		$artist_id = $passing[$b]['artist_id'];
+      		$name = $passing[$b]['name'];
+      		$album_id = $passing[$b]['album_id'];
+      		$album_title = $passing[$b]['album_title'];
+      		$track_id = $passing[$b]['track_id'];
+      		$track_title = $passing[$b]['track_title'];
+      		$track_duration = $passing[$b]['track_duration'];
         
       	$result = $database->query("SELECT artist_id FROM music WHERE track_id = '$track_id'");
       
@@ -50,7 +51,6 @@ for (i = 0; i <= count($queries); i++) { // Iterate through each element.
         	track_duration) VALUES ('$artist_id', '$name', '$album_id', '$album_title',
         	'$track_id', '$track_title', '$track_duration')");
 	}
-	array_pop($queries);
 }
 ?>
 
