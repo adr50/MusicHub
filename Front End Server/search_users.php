073f5770c4cd2ae99292e15d12afde295b3827e5
sbs97 <56939007+sbs97@users.noreply.github.com>
2019-12-04 09:36:53 -0500

    Update search_users.php

---
+++ Front End Server/search_users.php
@@ -0,0 +1,32 @@
+<?php
+if(isset($_GET["search"]))
+{
+	$search_query = $_GET["search"];
+	$search_terms = explode(" ", $search_query);
+	$conn_string = "";
+	$db = new PDO($conn_string, $root, $password);
+	$query = "select username from users";
+	
+	if(count($search_terms) > 0)
+	{
+		$query .= "where";
+		for($i = 0; $i < count($search_terms); $++){
+			if($i > 0){
+				$query .= " OR";
+			}
+			//TODO may need to adjust how %% are handled with placeholders
+			// %% may need to be inlcuded with the binded value
+			$query .= " name like %:term$i%";
+		}
+		$stmt = $db->prepare($query);
+		$i = 0;
+		foreach($search_terms as $term)
+		{
+			$stmt->bindValue(":term$i", $term);
+			$i++;
+		}
+		//Example resulting query "select * from MyTable where name like %one% OR name like %two% etc"
+		$r = $stmt->execute();
+		return $stmt->fetchAll();
+	}
+}
