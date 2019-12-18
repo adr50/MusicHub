6c8fbc06e33ca55280a7eed7f71f3aa6855c84ec
koliasoha <43789525+koliasoha@users.noreply.github.com>
2019-12-18 12:39:23 -0500

    Update scp.sh

--- scp.sh
+++ scp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #To run this script: chmod +x scp_migrate
 #Should have 2 scp files: to transfer files from Dev to Q/A and to transfer files from Q/A to Production.
-
-scp /home/MusicHub ipaddress:/home/hostname/MusicHub
+#Replace 
+scp /home/hostname/MusicHub ipaddress:/home/hostname/MusicHub
